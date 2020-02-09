e186c77fbfcdecea842fcd43edae893ef4ca13c3
BohdanRadchenko <radchenkobs@gmail.com>
2020-02-10 00:59:28 +0200

    Updates

---
+++ precache-manifest.90f0cfef3f38fe47926ba666f54edbf5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d40250e4d57d805c70bdbd3f5fcc462",
+    "url": "/dance_school/index.html"
+  },
+  {
+    "revision": "a6a8111591050f7bcb93",
+    "url": "/dance_school/static/css/main.4ad17416.chunk.css"
+  },
+  {
+    "revision": "49646330c44b0f34d685",
+    "url": "/dance_school/static/js/2.da8034f2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/dance_school/static/js/2.da8034f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a8111591050f7bcb93",
+    "url": "/dance_school/static/js/main.8679bdb8.chunk.js"
+  },
+  {
+    "revision": "d33d9ecf82d25b0c5667",
+    "url": "/dance_school/static/js/runtime-main.53c90acc.js"
+  },
+  {
+    "revision": "13c08ac8bdcfe259393dc9a42295aeb7",
+    "url": "/dance_school/static/media/bunner_img.13c08ac8.jpg"
+  },
+  {
+    "revision": "749ef0d60b7805a142a4f750ec083325",
+    "url": "/dance_school/static/media/flamenco.749ef0d6.jpg"
+  },
+  {
+    "revision": "b630e3955d4af335200c2a80f6e1ac96",
+    "url": "/dance_school/static/media/julia.b630e395.jpg"
+  },
+  {
+    "revision": "3152c2f6fff95fb00a334c9196c8b6a7",
+    "url": "/dance_school/static/media/salsa.3152c2f6.jpg"
+  }
+]);
